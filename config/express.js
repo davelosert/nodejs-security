fe7406b2a111ddf5951528d0bc0a3acfbd506e10
Dave <david.losert@gmx.de>
2014-04-21 17:16:06 +0200

    Replaced express 3.x with 4.0.0 and therfore added all now necessary middleware.

--- config/express.js
+++ config/express.js
@@ -2,20 +2,23 @@
  * Created by David on 16.03.14.
  */
 var express = require('express'),
-	path = require('path');
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	methodOverride = require('method-override'),
+	csrf = require('csurf');
 
 
 module.exports = function (app) {
 	app.set('port', process.env.PORT || 3000);
 	app.disable('x-powered-by'); // Disable the x-powered-by header in http request to prevent that an attacker knows your backend-framework easily.
-	app.use(express.logger('dev'));
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.methodOverride());
+	app.use(bodyParser());
+	app.use(methodOverride());
 
 	// Use the cookieParser BEFORE Session, since Session is using the cookie-parser.
-	app.use(express.cookieParser('s3cr3t'));
-	app.use(express.session({
+	app.use(cookieParser('s3cr3t'));
+	app.use(session({
 		secret: 's3cr3t',
 		key: 'sessionId', // use generic session-cookie name, else it would be "connectSid", revealing your used framework
 		cookie: {
@@ -27,7 +30,7 @@ module.exports = function (app) {
 
 	// CSRF-Measures. This does not help against get-methods since they´re not verified. So NEVER
 	// let a GET-Method modify the state of your app.
-	app.use(express.csrf());
+	app.use(csrf());
 	app.use(function (req, res, next) {
 		res.locals.csrfToken = req.csrfToken();
 		next();
@@ -38,17 +41,15 @@ module.exports = function (app) {
 	/**
 	 * Since we are using https, redirect all http-calls to https
 	 */
-	app.use(function (req, res, next) {
-		if (!req.secure) {
-			return res.redirect(['https://', req.get('Host'), req.url].join(''));
-		}
-		else {
-			next();
-		}
-	});
+//	app.use(function (req, res, next) {
+//		if (!req.secure) {
+//			return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//		}
+//		else {
+//			next();
+//		}
+//	});
 
-
-	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public'))); // deliver all js, imgs and css
 	app.use(express.static(path.join(__dirname, 'views'))); // just deliver the views
 };
