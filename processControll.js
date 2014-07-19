ad9d4f4b4a41af863bc2313539a202a9373dbbea
Dave <david.losert@gmx.de>
2014-07-19 18:34:46 +0200

    Deleted deprecated files.

--- processControll.js
+++ processControll.js
@@ -1,2 +1,17 @@
 // Basic packages
-var cp = request('child_process');
+var cp = require('child_process');
+
+
+var appSecure = cp.fork('./secureApp/server_secure.js', [], {
+	env: {
+		port: 3333
+	}
+});
+
+appSecure.on('error', function (error) {
+	console.error(error);
+});
+
+appSecure.on('exit', function (code, signal) {
+	console.log('App Secure was exited with code ' + code);
+});
