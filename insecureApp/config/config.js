04c4be6f550e3169035728e9723c128ede123eea
Dave <david.losert@gmx.de>
2014-07-24 14:26:26 +0200

    Added mongoDB Config to insecure app.

--- insecureApp/config/config.js
+++ insecureApp/config/config.js
@@ -1,17 +1,26 @@
 /**
- * Created with IntelliJ IDEA.
+ * Application related configuration like Database-Connections, Ports, Default-Values and whatsoever...
  * User: david.losert
  * Date: 18.07.2014
  * Time: 15:44
  */
 
+var config = {
+	'development': {
+		'mongoDB': {
+			host  : '192.168.2.105',
+			port  : 27017,
+			dbName: 'insecureApp'
+		}
+	},
+	'test'       : {
+		'mongoDB': {
+			host  : 'localhost',
+			port  : 27017,
+			dbName: 'insecureApp_Test'
 
-module.exports = function (env) {
-    if(env == 'dev'){
-        return {
-
-        }
-    } else if (env === 'PRODUCTION') {
-
-    }
+		}
+	}
 };
+
+module.exports = config[process.env.NODE_ENV || 'development'];
