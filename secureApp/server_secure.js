182eb771a8f726ac87402a021eda63fe5ae17ffb
Dave <david.losert@gmx.de>
2014-07-24 11:31:47 +0200

    Implemented automatic integration of models and created example user model.

--- secureApp/server_secure.js
+++ secureApp/server_secure.js
@@ -5,15 +5,30 @@
  * Time: 14:43
  */
 
-
-// Basic packages
+// *************************
+// Load Basic Packages & Config
+// *************************
 var express = require('express'),
 	http = require('http'),
 	url = require('url'),
+	fs = require('fs'),
+	path = require('path'),
+	_ = require('lodash'),
 	mongoose = require('mongoose');
 
 var config = require('./config/config');
 
+
+// *************************
+// DB CONNECTIONS
+// *************************
+// Load all Models
+var modelPath = 'model';
+_.each(fs.readdirSync(modelPath), function (fileName) {
+	require(path.resolve(path.join(modelPath, fileName)));
+});
+
+// Create URL
 var mongoURL = url.format({
 	protocol: 'mongodb',
 	slashes : true,
@@ -22,6 +37,7 @@ var mongoURL = url.format({
 	pathname: config.mongoDB.dbName
 });
 
+// Connect MongoDB
 mongoose.connect(mongoURL, function (err) {
 	if (err) {
 		console.error('Mongo-Connection Failed with error:', err);
@@ -31,13 +47,19 @@ mongoose.connect(mongoURL, function (err) {
 	}
 });
 
+
+// *************************
+// APPLICATION SETUP
+// *************************
 var appSecure = express();
 appSecure.set('port', process.env.port || 3334);
 
 require('./config/secure_express')(appSecure);
 require('./routes')(appSecure);
 
-// Create the secure server
+// *************************
+// APP SERVER INSTANCE
+// *************************
 module.exports = appSecure;
 appSecure.listen(appSecure.get('port'), function () {
 	console.log('Secure Server listening on port ' + appSecure.get('port'));
