9bec9d08838e21eebde1f9d4635496d4d5b61cbe
Dave <david.losert@gmx.de>
2014-07-24 11:48:47 +0200

    Synchronized db-connections into insecureApp.

--- processControll.js
+++ processControll.js
@@ -1,17 +1,31 @@
 // Basic packages
-var cp = require('child_process');
+var cp = require('child_process'),
+	path = require('path');
 
 
-var appSecure = cp.fork('./secureApp/server_secure.js', [], {
+var appSecure = cp.fork(path.join('secureApp', 'server_secure.js'), [], {
 	env: {
-		port: 3333
+		port    : 3334,
+		NODE_ENV: 'development'
 	}
 });
 
-appSecure.on('error', function (error) {
-	console.error(error);
+var appInsecure = cp.fork(path.join('insecureApp', 'server_insecure.js'), [], {
+	env: {
+		port    : 3335,
+		NODE_ENV: 'development'
+	}
 });
 
-appSecure.on('exit', function (code, signal) {
-	console.log('App Secure was exited with code ' + code);
-});
+integrateErrorHandling(appSecure);
+integrateErrorHandling(appInsecure);
+
+function integrateErrorHandling(app) {
+	app.on('error', function (error) {
+		console.error(error);
+	});
+
+	app.on('exit', function (code, signal) {
+		console.log('App Secure was exited with code ' + code);
+	});
+}
